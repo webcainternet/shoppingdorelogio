775ca387a02a6c52b2232f94c8666e8b573c1bcd
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-24 23:26:08 -0300

    up files

---
+++ public/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shoppingdorelogio.homolog.lojavirtual.digital/admin/');
+define('HTTP_CATALOG', 'http://shoppingdorelogio.homolog.lojavirtual.digital/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shoppingdorelogio.homolog.lojavirtual.digital/admin/');
+define('HTTPS_CATALOG', 'http://shoppingdorelogio.homolog.lojavirtual.digital/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/shoppingdorelogio/public/admin/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/shoppingdorelogio/public/system/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/shoppingdorelogio/public/admin/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/shoppingdorelogio/public/admin/view/template/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/shoppingdorelogio/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/shoppingdorelogio/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/shoppingdorelogio/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/shoppingdorelogio/public/system/download/');
+define('DIR_UPLOAD', '/Users/fernandomendes/github/shoppingdorelogio/public/system/upload/');
+define('DIR_LOGS', '/Users/fernandomendes/github/shoppingdorelogio/public/system/logs/');
+define('DIR_MODIFICATION', '/Users/fernandomendes/github/shoppingdorelogio/public/system/modification/');
+define('DIR_CATALOG', '/Users/fernandomendes/github/shoppingdorelogio/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'shoppingdorelogio');
+define('DB_PREFIX', 'oc_');
---
+++ public/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shoppingdorelogio.homolog.lojavirtual.digital/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shoppingdorelogio.homolog.lojavirtual.digital/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/shoppingdorelogio/public/catalog/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/shoppingdorelogio/public/system/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/shoppingdorelogio/public/catalog/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/shoppingdorelogio/public/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/shoppingdorelogio/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/shoppingdorelogio/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/shoppingdorelogio/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/shoppingdorelogio/public/system/download/');
+define('DIR_UPLOAD', '/Users/fernandomendes/github/shoppingdorelogio/public/system/upload/');
+define('DIR_MODIFICATION', '/Users/fernandomendes/github/shoppingdorelogio/public/system/modification/');
+define('DIR_LOGS', '/Users/fernandomendes/github/shoppingdorelogio/public/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'shoppingdorelogio');
+define('DB_PREFIX', 'oc_');
